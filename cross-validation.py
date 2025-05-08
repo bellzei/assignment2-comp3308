b147c62c760cb17ca62be32a107d8302f7d4ca42
bellzei <143505986+bellzei@users.noreply.github.com>
2025-05-08 10:05:26 +1000

    files

---
+++ cross-validation.py
@@ -0,0 +1,4 @@
+# using the folds i just created, make one testing data and the rest training data
+# run our algorithms on them - naive bayes + knn for a few different values of k
+# evaluate accuracy and any other measures we deem important for our report
+# 
